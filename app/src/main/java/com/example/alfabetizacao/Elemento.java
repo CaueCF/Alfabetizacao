f208a556f2bcd90a14659d518290a47d1ae81970
Cauê Castello Ferreira <cauecferreira@gmail.com>
2022-05-26 14:45:30 -0300

    26-05

---
+++ app/src/main/java/com/example/alfabetizacao/Elemento.java
@@ -0,0 +1,25 @@
+package com.example.alfabetizacao;
+
+public class Elemento {
+
+    private int imagem;
+    private String textoImagem;
+
+    public String getTextoImagem() {
+        return textoImagem;
+    }
+
+    public void setTextoImagem(String textoImagem) {
+        this.textoImagem = textoImagem;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+
+
+}
--- app/src/main/java/com/example/alfabetizacao/MainActivity.java
+++ app/src/main/java/com/example/alfabetizacao/MainActivity.java
@@ -3,13 +3,40 @@ package com.example.alfabetizacao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //public ArrayList<Button> btns;
+    private Button l1, l2, l3, l4;
+    private TextView palavra;
+    private ArrayList<Elemento> imagens;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imagens = new ArrayList<Elemento>();
+        carregaElemento();
+
+
     }
+
+    public void carregaElemento(){
+        Elemento e1 = new Elemento();
+        e1.setImagem(R.drawable.casa);
+        e1.setTextoImagem("Casa");
+        imagens.add(e1);
+
+
+
+    }
+
+
 }
