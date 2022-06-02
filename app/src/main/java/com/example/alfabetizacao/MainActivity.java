cb73e89c58c9d3183fc5ce11ff31c996edfe2531
Cauê Castello Ferreira <cauecferreira@gmail.com>
2022-06-02 16:34:22 -0300

    .

--- app/src/main/java/com/example/alfabetizacao/MainActivity.java
+++ app/src/main/java/com/example/alfabetizacao/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,19 +15,18 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ArrayList<Button> btns;
-    private Button l1, l2, l3, l4;
-    private TextView palavra;
+    private Button principal;
     private ArrayList<Elemento> imagens;
-    private LinearLayout imagem, botoes;
-
+    private LinearLayout botoes, text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imagem = (LinearLayout) findViewById(R.id.layoutImagem);
         botoes = (LinearLayout) findViewById(R.id.layoutBotoes);
+        text =  (LinearLayout) findViewById(R.id.layoutText);
+        principal = (Button) findViewById(R.id.buttonImagem);
 
         imagens = new ArrayList<Elemento>();
         carregaElemento();
@@ -40,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Collections.shuffle(btns);
 
+        carregaJogo();
+
     }
 
     public void carregaElemento(){
@@ -48,6 +50,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         e1.setTextoImagem("Casa");
         imagens.add(e1);
     }
+
+    public void carregaJogo(){
+
+        principal.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
+                getResources().getDrawable(imagens.get(0).getImagem()),null, null);
+
+        int i = 0;
+        for(Button b: btns){
+
+            botoes.addView(b);
+            TextView t = new TextView(this);
+            t.setId(i);
+            b.setId(i);
+            text.addView(t);
+            i++;
+        }
+    }
+
 
 
     @Override
