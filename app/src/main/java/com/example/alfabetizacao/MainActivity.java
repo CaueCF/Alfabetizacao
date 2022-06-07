1ff91c39f0d22aac713f66a82ab6e7836105c770
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2022-06-07 11:39:39 -0300

    imagens

--- app/src/main/java/com/example/alfabetizacao/MainActivity.java
+++ app/src/main/java/com/example/alfabetizacao/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         imagens = new ArrayList<Elemento>();
         carregaElemento();
+        Collections.shuffle(imagens);
 
         btns = new ArrayList<Button>();
 
@@ -45,7 +46,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         e1.setImagem(R.drawable.casa);
         e1.setTextoImagem("Casa");
         imagens.add(e1);
+
+        Elemento e2 = new Elemento();
+        e2.setImagem(R.drawable.pato);
+        e2.setTextoImagem("Pato");
+        imagens.add(e2);
+
+        Elemento e3 = new Elemento();
+        e3.setImagem(R.drawable.vaca);
+        e3.setTextoImagem("Vaca");
+        imagens.add(e3);
+
+        Elemento e4 = new Elemento();
+        e4.setImagem(R.drawable.mesa);
+        e4.setTextoImagem("Mesa");
+        imagens.add(e4);
+
+        Elemento e5 = new Elemento();
+        e5.setImagem(R.drawable.lobo);
+        e5.setTextoImagem("Lobo");
+        imagens.add(e5);
+
+        Elemento e6 = new Elemento();
+        e6.setImagem(R.drawable.osso);
+        e6.setTextoImagem("Osso");
+        imagens.add(e6);
+
+        Elemento e7 = new Elemento();
+        e7.setImagem(R.drawable.bola);
+        e7.setTextoImagem("Bola");
+        imagens.add(e7);
+
+        Elemento e8 = new Elemento();
+        e8.setImagem(R.drawable.copo);
+        e8.setTextoImagem("Copo");
+        imagens.add(e8);
+
+        Elemento e9 = new Elemento();
+        e9.setImagem(R.drawable.gato);
+        e9.setTextoImagem("Gato");
+        imagens.add(e9);
+
+        Elemento e10 = new Elemento();
+        e10.setImagem(R.drawable.urso);
+        e10.setTextoImagem("Urso");
+        imagens.add(e10);
     }
+
+
 
     public void carregaJogo(){
 
